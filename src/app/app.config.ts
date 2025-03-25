263732843610f59eabe45649553a079c70fc26d5
eulabconsult <eulabconsult@gmail.com>
2025-03-25 21:41:34 +0100

    Install Tools

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,62 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  isDevMode,
+  importProvidersFrom,
+  LOCALE_ID,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MessageService } from 'primeng/api';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+
+import localeItSM from '@angular/common/locales/it-SM';
+
+import { registerLocaleData } from '@angular/common';
+import theme from './theme/theme';
+import * as it from './locale/primeng.json';
+
+registerLocaleData(localeItSM);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    { provide: LOCALE_ID, useValue: 'it-SM' },
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([])
+    ),
+    importProvidersFrom([BrowserAnimationsModule]),
+    MessageService,
+    provideAnimationsAsync(),
+    providePrimeNG({
+      ripple: true,
+      theme: {
+        preset: theme,
+        options: {
+          darkModeSelector: '.dark',
+        },
+      },
+      translation: {
+        ...it,
+      },
+    }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
+  ],
 };
